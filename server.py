94dcf1b715386815c8eb9c43375b81206f44f357
NinjaNekoSama <purushothamkoduride@gmail.com>
2021-09-15 14:34:23 +0530

    Added environment variables

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 import requests
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    blog_url = "https://api.npoint.io/c95540ed65ed0976dbe1"
+    blog_url = os.environ['My_Content']
     response = requests.get(blog_url)
     all_posts = response.json()
     return render_template("index.html",posts=all_posts)
